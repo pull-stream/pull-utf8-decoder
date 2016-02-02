b2c822f19c65e433c3ef426042f8cedb4db6b153
Dominic Tarr <dominic.tarr@gmail.com>
2016-02-02 23:04:22 +1300

    accept encoding, and cb error asap

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 
 var Decode = require('string_decoder').StringDecoder
 
-module.exports = function () {
-  var decoder = new Decode('utf-8'), ended
+module.exports = function (enc) {
+  var decoder = new Decode(enc || 'utf-8'), ended
   return function (read) {
     return function (abort, cb) {
       if(ended) return cb(ended)
       read(abort, function (end, data) {
-        if(ended = end) {
-          var _data = decoder.end()
-          cb(null, _data)
-        } else {
-          var _data = decoder.write(data)
-          cb(null, _data)
-        }
+        if(ended = end && end !== true)
+          cb(end)
+        else if(end)
+          cb(null, decoder.end())
+        else
+          cb(null, decoder.write(data))
       })
     }
   }
 }
+
+
 
